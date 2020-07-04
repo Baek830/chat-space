64f1dc0473cd12f35d25299b3d3554433e3e29d9
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-07-05 08:28:32 +0900

    edit member search

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,7 +5,7 @@ $(function() {
     var html = `
               <div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn-add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
               </div>
               `;
     search_list.append(html);
@@ -30,8 +30,8 @@ $(function() {
     let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
     $(`#${userId}`).append(html);
   }
-  $(".chat-group-form__input").on("keyup", function(){
-    var input = $(".chat-group-form__input").val();
+  $("#user-search-field").on("keyup", function(){
+    var input = $("#user-search-field").val();
     $.ajax({
       type: 'GET',
       url: '/users',
@@ -54,7 +54,7 @@ $(function() {
       alert('ユーザーが表示できません。');
     });
   });
-  $(document).on('click','.chat-group-user__btn-add',function(){
+  $(document).on('click','.chat-group-user__btn--add',function(){
     console.log
     const userName = $(this).attr("data-user-name");
     const userId = $(this).attr("data-user-id");
